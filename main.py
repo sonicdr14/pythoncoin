de930e3c679ad56fa22d3676e95c6069c92eef6c
sonicdr14 <159570728+sonicdr14@users.noreply.github.com>
2024-02-12 10:52:04 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,26 @@
+def on_button_pressed_a():
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_a():
+    if Math.random_boolean():
+        pass
+    else:
+        pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_a():
+    if Math.random_boolean():
+        basic.show_icon(IconNames.SKULL)
+    else:
+        basic.show_icon(IconNames.SQUARE)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_a():
+    basic.show_icon(IconNames.DIAMOND)
+    basic.show_icon(IconNames.SMALL_DIAMOND)
+    basic.show_icon(IconNames.DIAMOND)
+    basic.show_icon(IconNames.SMALL_DIAMOND)
+    if Math.random_boolean():
+        basic.show_icon(IconNames.SKULL)
+    else:
+        basic.show_icon(IconNames.SQUARE)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
